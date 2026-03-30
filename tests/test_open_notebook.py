b8a78e366fdf2daae3183c09b0460fb3989c0b17
wpk-nist-gov <5348008+wpk-nist-gov@users.noreply.github.com>
2026-03-30 14:32:35 +0000

    chore(template): accept new copier update [skip ci]

--- tests/test_open_notebook.py
+++ tests/test_open_notebook.py
@@ -1,7 +1,40 @@
 """Tests for `open-notebook` package."""
 
+<<<<<<< before updating
 from open_notebook import __version__
 
 
 def test_version() -> None:
     assert isinstance(__version__, str)
+=======
+from __future__ import annotations
+
+import re
+
+import pytest
+
+from open_notebook import example_function
+
+
+def test_version() -> None:
+    from open_notebook import __version__
+
+    assert isinstance(__version__, str)
+    assert re.match(r"^\d+\.\d+\.\d+.*$", __version__) is not None
+
+
+@pytest.fixture
+def response() -> tuple[int, int]:
+    return 1, 2
+
+
+def test_example_function(response: tuple[int, int]) -> None:
+    expected = 3
+    assert example_function(*response) == expected
+
+
+def test_command_line_interface() -> None:
+    from open_notebook import cli
+
+    assert not cli.main([])
+>>>>>>> after updating
