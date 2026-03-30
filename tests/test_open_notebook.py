e2f862945abc259bfb1fc370ba6a602c6622bbb6
wpk-nist-gov <wpk@nist.gov>
2026-03-30 11:14:44 -0400

    chore(template): accept new copier update (#40)
    Co-authored-by: wpk-nist-gov <5348008+wpk-nist-gov@users.noreply.github.com>

--- tests/test_open_notebook.py
+++ tests/test_open_notebook.py
@@ -1,7 +1,12 @@
 """Tests for `open-notebook` package."""
 
-from open_notebook import __version__
+from __future__ import annotations
+
+import re
 
 
 def test_version() -> None:
+    from open_notebook import __version__
+
     assert isinstance(__version__, str)
+    assert re.match(r"^\d+\.\d+\.\d+.*$", __version__) is not None
